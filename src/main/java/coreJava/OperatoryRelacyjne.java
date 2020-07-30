84b6486eca0c4d20257994293a6062ff76fbdfa4
Dominik <krypel.dominik@gmail.com>
2020-07-30 12:19:18 +0200

    OperatoryRelacyjne

---
+++ src/main/java/coreJava/OperatoryRelacyjne.java
@@ -0,0 +1,32 @@
+package coreJava;
+
+public class OperatoryRelacyjne {
+
+    public static void main (String[] args) {
+
+        /*
+        OPERATOR RALACYJNY (INACZEJ OPERATOR PORÓWNANIA)
+
+        == -  To jest porównanie , jeden znak "=" to jest przypisanie
+        != -
+        >   - większe od
+        <   - mniejsze od
+        >=  - większe bądź równe
+        <=  - mniejsze bądź równe
+        
+         */
+
+        // opis operatora bollean
+        boolean isTrue = 4 == 4;
+        System.out.println("Porównanie wartości : " + isTrue);
+
+        //// opis operatora !=
+        int a = 4;
+        int b = 6;
+        boolean isTrue2 = a != b;
+
+        System.out.println("Czy wartości są różne : " + isTrue2);
+
+
+    }
+}
