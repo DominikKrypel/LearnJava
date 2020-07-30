5cd517e459f9dcd94f05a9f8709317e44c95e75d
Dominik <krypel.dominik@gmail.com>
2020-07-30 13:56:23 +0200

    Operatory logiczne

--- src/main/java/coreJava/OperatoryLogiczne.java
+++ src/main/java/coreJava/OperatoryLogiczne.java
@@ -4,7 +4,54 @@ public class OperatoryLogiczne {
 
     public static void main (String[] args) {
 
+        /*
+        Operatory Logiczne :
+
+        !         - negacja
+        !(true)   - false
+        !(false)  - true
+        &&        - koniunkcja, i to jest rezprezentowane przez nasze polskie słowo "I" i oznacza "w tym samym momencie"
+                    true  true   - true
+                    true  false  - false
+                    false true   - false
+                    false false  - false
+                    KONIUNKCJA JEST PRAWDZIWA TYLKO GDY OBA WYRAŻENIA SĄ PRAWDZIWE
+
+        ||        - alternatywa - "lub" - to lub też to
+                    true  true   - true
+                    true  false  - true
+                    false true   - true
+                    false false  - false
+                    ALTERNATYWA JEST FAŁCZYWA TYLKO GDY OBA WYRAŻENIA SĄ FAŁSZYWE
+         */
 
 
+        //Przykłąd negacji
+
+       int a = 3;
+       int b = 5;
+       int c = 6;
+
+        if (!(a == b))
+            System.out.println("Czy a nie jest równe b : TAK");
+        else {
+            System.out.println("Czy a nie jest równe b : NIE");
+        }
+
+        //Przykład koniunkcji
+
+        if (a == b && c > 7)
+            System.out.println("Koniunkcja, czy spełniam dwa wyrażenia : Tak");
+        else {
+            System.out.println("Koniunkcja, czy spełniam dwa wyrażenia : Nie");
+        }
+
+        //Przykład alternatywy
+
+        if (a == b || c > 7)
+            System.out.println("Alternatywa, czy spełniam przynajmniej jedno wyrażenie : Tak");
+        else {
+            System.out.println("Alternatywa, czy spełniam przynajmniej jedno wyrażenie : Nie");
+        }
     }
 }
