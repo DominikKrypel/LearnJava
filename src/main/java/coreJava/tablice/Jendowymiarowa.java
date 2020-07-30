5f1f452afec1286ee4df808f83ecde188fa93f48
Dominik <krypel.dominik@gmail.com>
2020-07-30 15:12:38 +0200

    AddTables

---
+++ src/main/java/coreJava/tablice/Jendowymiarowa.java
@@ -0,0 +1,9 @@
+package coreJava.tablice;
+
+public class Jendowymiarowa {
+
+    public static void main (String[] args) {
+
+
+    }
+}
---
+++ src/main/java/coreJava/tablice/Wielowymiarowa.java
@@ -0,0 +1,9 @@
+package coreJava.tablice;
+
+public class Wielowymiarowa {
+
+    public static void main (String[] args) {
+
+
+    }
+}
