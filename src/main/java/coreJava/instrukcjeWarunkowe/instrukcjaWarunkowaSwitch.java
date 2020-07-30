058a9d86a488c9d90f0bca2139992b7a2cead42f
Dominik <krypel.dominik@gmail.com>
2020-07-30 15:05:20 +0200

    InstrukcjaWarunkowaSwitch update

--- src/main/java/coreJava/instrukcjeWarunkowe/instrukcjaWarunkowaSwitch.java
+++ src/main/java/coreJava/instrukcjeWarunkowe/instrukcjaWarunkowaSwitch.java
@@ -2,14 +2,38 @@ package coreJava.instrukcjeWarunkowe;
 
 public class instrukcjaWarunkowaSwitch {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         /*
-        Switch
-
-        
+        Switch (przełącznik)
 
          */
 
+        int a = 50;
+        switch (a)       // instrukcja "switch" jest przerywana
+        {
+            case 50:
+                System.out.println("a jest równe 50");
+                break; // wyjście z przełącznika "switch", jeżeli by nie było słowa "break" to by się wykonały poniższe instrukcje
+            case 100:
+                System.out.println("a jest równe 100");
+                break;
+            default: // domyślnie
+                System.out.println("a jest inną wartością : " + a);
+        }
+
+        System.out.println("*****************************");
+
+        switch (a)       // instrukcja "switch" nie jest przerywana
+        {
+            case 50:
+                System.out.println("a jest równe 50");
+            case 100:
+                System.out.println("a jest równe 100");
+            default: // domyślnie
+                System.out.println("a jest inną wartością : " + a);
+
+
+        }
     }
 }
