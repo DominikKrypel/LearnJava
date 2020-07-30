5ad18aa0623eff9db6940e37b2a6d55e278cf335
Dominik <krypel.dominik@gmail.com>
2020-07-30 11:30:01 +0200

    Rzutowanie

---
+++ src/main/java/coreJava/Rzutowanie.java
@@ -0,0 +1,19 @@
+package coreJava;
+
+public class Rzutowanie {
+
+    public static void main (String[] args) {
+
+             /*
+        Rzytowanie służy do zmiany na inny typ zmiennej
+         */
+
+        int a = 5,  b = 2; // po przecinku możemy deklarować kilka na raz
+        double c = 12, d = 15;
+
+        System.out.println((double)a / b); // zmieniliśmy typ zmiennej "a" na double , wykonaliśmy rzutowanie
+
+
+
+    }
+}
