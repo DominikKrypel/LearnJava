9a63bc17b3e02217838334171ef5aeedfa3e91d5
Dominik <krypel.dominik@gmail.com>
2020-08-03 11:05:11 +0200

    KeywordThis

--- src/main/java/coreJava/programowanieObiektowe/KeywordThis.java
+++ src/main/java/coreJava/programowanieObiektowe/KeywordThis.java
@@ -2,26 +2,43 @@ package coreJava.programowanieObiektowe;
 
 public class KeywordThis {
 
-    int a = 2; // w klasie używam zmiennej "a" do której mam przypisaną wartość 2
+    public static void main(String[] args) {
 
+        Punkt p = new Punkt(35, 50);
 
-public void getData(){
-    int a = 3; /* w tej metodzie używam zmiennej "a" do której mam przypisaną wartość 3 i to jest lokalna zmienna, która żyje
-    w tej metodzie */
-    System.out.println(a);
-
-    // w bloku getData chcę wydrukować zmienną a do której jest przypisana wartość 2.
-    // this odnosi się do bieżącego obiektu, który jest na poziomie klasy
-
-}
-
-    public static void main(String[] args){
-
-    KeywordThis kt = new KeywordThis();
-        kt.getData();
-
+        System.out.println("p.x : " + p.x);
+        System.out.println("p.y : " + p.y);
 
     }
+
+    static class Punkt {
+
+        Punkt(int x, int y) {   //parametry
+
+            System.out.println("Wywołano konstruktor z dwoma parametrami");
+
+
+            this.x = x;      // bez słowa kluczowego : this kompilator się myli czy chodzi o poniższe zmienne z klasy Punkt, czy chodzi o parametry
+            this.y = y;
+
+            /*
+
+            this(p).x(35 linia) = x;
+            this(p).y (36 linia)= y;
+
+            tzn : 'this' zmienia swoją postać w zależności od tego na jakim obiekcie operujesz. 'this' to obiekt na którym aktualnie operujesz
+
+             */
+
+
+        }
+
+        int x;          // poniższe zmienne z klasy Punkt
+        int y;
+
+    }
+
+
 
 
 }
