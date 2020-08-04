dd6dce535127aadf25745e148a059ab219174b82
Dominik <krypel.dominik@gmail.com>
2020-08-04 14:37:38 +0200

    update

---
+++ src/main/java/coreJava/programowanieObiektowe/KeywordFinal.java
@@ -0,0 +1,20 @@
+package coreJava.programowanieObiektowe;
+
+import java.util.Date;
+
+public class KeywordFinal {
+
+    public static void mainf (String[] args){
+
+        final double PI = 3.14;                 //final wstawia się, żeby przypadki nikt tego nie zmienił
+
+    }
+
+}
+    /*
+    Parę informacji o keywordzie final :
+
+    1. Nie można rozszerzać klasy final
+
+
+     */
